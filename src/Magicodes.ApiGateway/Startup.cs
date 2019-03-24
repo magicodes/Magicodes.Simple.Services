e10a3600fda3675700fc96fc79d48f753f97b59d
李文强 <codelove1314@live.cn>
2019-03-24 15:32:23 +0800

    修改配置和脑图

--- src/Magicodes.ApiGateway/Startup.cs
+++ src/Magicodes.ApiGateway/Startup.cs
@@ -7,9 +7,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Administration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
+using Ocelot.Cache.CacheManager;
+using Ocelot.Provider.Polly;
 
 namespace Magicodes.ApiGateway.Host
 {
@@ -26,9 +29,33 @@ namespace Magicodes.ApiGateway.Host
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Identity Server Bearer Tokens
+            services.AddAuthentication().AddIdentityServerAuthentication("IdentityBearer", options =>
+            {
+                options.Authority = "http://localhost:2000/";
+                options.RequireHttpsMetadata = false;
+                options.ApiName = "client";
+                options.ApiSecret = "def2edf7-5d42-4edc-a84a-30136c340e13";
+                options.SupportedTokens = IdentityServer4.AccessTokenValidation.SupportedTokens.Both;
+            });
+
             services
                 .AddOcelot()
-                .AddConsul();
+                .AddConsul()
+                //启用缓存
+                .AddCacheManager(x =>
+                {
+                    x.WithDictionaryHandle();
+                })
+                .AddPolly()
+                .AddAdministration("/administration", options=>
+                {
+                    options.Authority = "http://localhost:2000/";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "admin";
+                    options.ApiSecret = "07ABB7E9-233B-4ABC-83AB-7D191F7BBF7E";
+                    options.SupportedTokens = IdentityServer4.AccessTokenValidation.SupportedTokens.Both;
+                }); 
 
         }
 
--- src/Services.Test1/Startup.cs
+++ src/Services.Test1/Startup.cs
@@ -25,6 +25,8 @@ namespace Services.Test1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
